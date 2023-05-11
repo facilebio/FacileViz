0f8ca608bf5aedf2a56e5f92793c0529c90662cd
Steve Lianoglou <slianoglou@mazetx.com>
2023-05-10 19:41:26 -0700

    Tweak to 'unselected()' to support length() > 1 inputs

--- R/utils.R
+++ R/utils.R
@@ -6,17 +6,20 @@
 #' NOTE: This is really a function that is used by shiny modules, but instead
 #' of having every shiny function check if something is "unselected" and setting
 #' it to NULL, we move that functionality in here so that internal vizualization
-#' functions can do that jusst once
+#' functions can do that just once
 #'
 #' @export
 #' @param value The (character) object returned from a `selectInput`
+#' @return a logical (TRUE/FALSE) indicating if something is "selected"
 unselected <- function(value, ignore = c("---", "__initializing__", "")) {
   if (is.null(value)) return(TRUE)
   # Otherwise this is a character
   if (is(value, "data.frame") || is(value, "tbl")) {
     val <- nrow(value) == 0
   } else {
-    val <- length(value) == 0L || nchar(value) == 0L || all(value %in% ignore)
+    val <- length(value) == 0L || 
+      all(sapply(value, function(v) nchar(v) == 0L)) || 
+      all(value %in% ignore)
   }
   val
 }
